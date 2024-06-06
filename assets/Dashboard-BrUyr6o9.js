0b567722052437880f7d926c7fa145ac55882ad2
RafiKK98 <rafikk1998@gmail.com>
2024-06-06 13:20:21 +0600

    Updates

---
+++ assets/Dashboard-BrUyr6o9.js
@@ -0,0 +1,1 @@
+import{j as r,B as a}from"./index-BqVk5qR8.js";import{T as o}from"./Typography-BUZHWWhv.js";const p=()=>r.jsx(a,{pt:6.25,children:r.jsx(o,{variant:"h2",children:"Dashboard"})});export{p as default};
