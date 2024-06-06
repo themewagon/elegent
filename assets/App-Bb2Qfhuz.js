cea49066a703a8afdc1c73e5f7159f3697855f4f
RafiKK98 <rafikk1998@gmail.com>
2024-06-06 15:10:51 +0600

    Updates

--- assets/App-Bb2Qfhuz.js
+++
@@ -1,1 +0,0 @@
-import{j as t,O as s}from"./index-BqVk5qR8.js";const p=()=>t.jsx(s,{});export{p as default};
---
+++ assets/App-C2o_5kbF.js
@@ -0,0 +1,1 @@
+import{j as t,O as s}from"./index-kmX9yFhP.js";const p=()=>t.jsx(s,{});export{p as default};
