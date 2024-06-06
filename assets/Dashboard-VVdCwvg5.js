289b472eebab93651ed7d36e7aece7546a0c22cd
RafiKK98 <rafikk1998@gmail.com>
2024-06-06 16:56:05 +0600

    Updates

---
+++ assets/Dashboard-VVdCwvg5.js
@@ -0,0 +1,1 @@
+import{j as r,B as a}from"./index-CF-tCSnD.js";import{T as o}from"./Typography-D7u6duG7.js";const p=()=>r.jsx(a,{pt:6.25,children:r.jsx(o,{variant:"h2",children:"Dashboard"})});export{p as default};
