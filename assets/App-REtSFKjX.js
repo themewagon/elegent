efab593ccdb40743e12ec451e6501b2c58959c79
RafiKK98 <rafikk1998@gmail.com>
2024-06-09 14:47:47 +0600

    Updates

--- assets/App-REtSFKjX.js
+++
@@ -1,1 +0,0 @@
-import{j as t,O as s}from"./index-Di0N_jsG.js";const p=()=>t.jsx(s,{});export{p as default};
---
+++ assets/Dashboard-DbALpbC5.js
@@ -0,0 +1,1 @@
+import{j as r,B as a}from"./index-aAOBF82i.js";import{T as o}from"./Typography-Ca1lqXgR.js";const p=()=>r.jsx(a,{pt:6.25,children:r.jsx(o,{variant:"h2",children:"Dashboard"})});export{p as default};
