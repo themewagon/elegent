9d6a133d9c55ce98b49d543dd45c72f1b0e47efa
RafiKK98 <rafikk1998@gmail.com>
2024-06-09 15:10:48 +0600

    Updates

--- assets/App-D0X6E-HT.js
+++
@@ -1,1 +0,0 @@
-import{j as t,O as s}from"./index-aAOBF82i.js";const p=()=>t.jsx(s,{});export{p as default};
---
+++ assets/Dashboard-dgM7FU6G.js
@@ -0,0 +1,1 @@
+import{j as r,B as a}from"./index-BbRgw_qd.js";import{T as o}from"./Typography-CDwbh19v.js";const p=()=>r.jsx(a,{pt:6.25,children:r.jsx(o,{variant:"h2",children:"Dashboard"})});export{p as default};
