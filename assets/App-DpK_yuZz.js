56eadfd6f4ec81e00fb6d172ca44034213d5a6e2
RafiKK98 <rafikk1998@gmail.com>
2024-06-10 14:20:15 +0600

    Updates

--- assets/App-DpK_yuZz.js
+++
@@ -1,1 +0,0 @@
-import{j as t,O as s}from"./index-C_8russW.js";const p=()=>t.jsx(s,{});export{p as default};
---
+++ assets/App-rn1r6PUO.js
@@ -0,0 +1,1 @@
+import{j as t,O as s}from"./index-DvDkV9MS.js";const p=()=>t.jsx(s,{});export{p as default};
